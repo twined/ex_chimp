c5178e20b3c7068635b65deff8f180b917952f1a
T. Mjoen <trond@twined.net>
2020-10-20 19:14:53 +0200

    Fix test

--- test/api/list_test.exs
+++ test/api/list_test.exs
@@ -2,6 +2,8 @@ defmodule ExChimp.ListTest do
   use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
+  alias ExChimp.List
+
   setup_all do
     HTTPoison.start()
   end
@@ -11,37 +13,41 @@ defmodule ExChimp.ListTest do
   @list_members_success_json File.read!(Path.join(@fixtures_dir, "list_members.json"))
   @list_add_member_success_json File.read!(Path.join(@fixtures_dir, "list_member_add.json"))
   @lists_query_string "count=100&fields=lists.id%2Clists.name%2Clists.stats.member_count&offset=0"
+  @test_email "urist.mcvankab+3@freddiesjokes.com"
+
+  @list_url "https://us12.api.mailchimp.com/3.0/lists?#{@lists_query_string}"
+  @upsert_url "https://us12.api.mailchimp.com/3.0/lists/asdf1234/members/852aaa9532cb36adfb5e9fef7a4206a9"
+  @list_members_url "https://us12.api.mailchimp.com/3.0/lists/asdf1234/members"
 
   test "get lists success" do
     use_cassette :stub,
-      url: "https://us12.api.mailchimp.com/3.0/lists?#{@lists_query_string}",
+      url: @list_url,
       method: "get",
       status_code: ["HTTP/1.1", 200, "OK"],
       body: @lists_success_json do
-      {:ok, body} = ExChimp.List.all()
+      {:ok, body} = List.all()
       assert body == Map.get(Jason.decode!(@lists_success_json), "lists")
     end
   end
 
   test "get list members" do
     use_cassette :stub,
-      url: "https://us12.api.mailchimp.com/3.0/lists/asdf1234/members",
+      url: @list_members_url,
       method: "get",
       status_code: ["HTTP/1.1", 200, "OK"],
       body: @list_members_success_json do
-      {:ok, body} = ExChimp.List.members("asdf1234")
+      {:ok, body} = List.members("asdf1234")
       assert body == Jason.decode!(@list_members_success_json)
     end
   end
 
   test "add member to list" do
     use_cassette :stub,
-      url: "https://us12.api.mailchimp.com/3.0/lists/asdf1234/members",
+      url: @upsert_url,
       method: "put",
       status_code: ["HTTP/1.1", 200, "OK"],
       body: @list_add_member_success_json do
-      {:ok, body} =
-        ExChimp.List.add_member("asdf1234", :subscribed, "urist.mcvankab+3@freddiesjokes.com")
+      {:ok, body} = List.add_member("asdf1234", :subscribed, @test_email)
 
       assert body == Jason.decode!(@list_add_member_success_json)
     end
